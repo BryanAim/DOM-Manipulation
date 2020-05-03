6347313a9f71122feb18d415aa992fccc34df119
Brian Isale <bryanaim00@gmail.com>
2020-05-03 13:25:18 +0000

    Targetting nodes with selectors

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('#container');
+
+console.dir(container.firstElementChild);
+
+const controls = document.querySelector('.controls');
+
+console.dir(controls.previousElementSibling);
+
+
